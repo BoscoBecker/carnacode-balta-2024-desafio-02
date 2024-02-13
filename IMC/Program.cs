fbcc1571841acf576cc57eeb6dbffef7c8c024b8
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-02-12 23:34:39 -0300

    Add files
    Adicionando Arquvos e solução a pasta

---
+++ IMC/Program.cs
@@ -0,0 +1,27 @@
+//using IMC.Client.Pages;
+using IMC.Components;
+using IMC.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents();
+builder.Services.AddSingleton<ServicesIMC>(); 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseWebAssemblyDebugging();
+else
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapRazorComponents<App>()
+   .AddInteractiveWebAssemblyRenderMode()
+   .AddInteractiveServerRenderMode();
+app.Run();
